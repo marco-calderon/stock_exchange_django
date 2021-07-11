5025142d86c24d17f5c2f67e91bb5560acf94fdb
Marco Calderon <mcalderon.code@gmail.com>
2021-07-10 17:04:41 -0700

    Fixed issues for getting prev rate and difference

--- stock_app/schema.py
+++ stock_app/schema.py
@@ -16,16 +16,22 @@ class RecordType(DjangoObjectType):
         return self.currency_code[2:5]
 
     def resolve_prev_rate(self, info):
-        start_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
+        start_date = self.date.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
         end_date = start_date + timedelta(days=1)
-        prev_record = Record.objects.first(date__gte=start_date, date__lte=end_date, currency_code=self.currency_code)
-        return prev_record.rate
+        prev_record = Record.objects.filter(date__gte=start_date, date__lte=end_date, currency_code=self.currency_code)
+        if len(prev_record) > 0:
+            return prev_record[0].rate
+        else:
+            return 0
 
     def resolve_difference(self, info):
-        start_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
+        start_date = self.date.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
         end_date = start_date + timedelta(days=1)
-        prev_record = Record.objects.first(date__gte=start_date, date__lte=end_date, currency_code=self.currency_code)
-        return self.rate - prev_record.rate
+        prev_record = Record.objects.filter(date__gte=start_date, date__lte=end_date, currency_code=self.currency_code)
+        if len(prev_record) > 0:
+            return self.rate - prev_record[0].rate
+        else:
+            return 0
 
 class Query(ObjectType):
     week_records = List(RecordType, code_from=String(default_value="USD"), code_to=String(default_value="MXN"))
@@ -40,17 +46,13 @@ class Query(ObjectType):
     def resolve_week_records(root, info, code_from, code_to):
         start_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=7)
         end_date = start_date + timedelta(days=8)
-
         week_records = Record.objects.filter(date__gte=start_date, date__lte=end_date, currency_code="('{0}',)".format(code_to))
-
         return week_records
 
     def resolve_comparison_records(root, info, code_from, code_to, start_timestamp, end_timestamp):
         start_date = datetime.fromtimestamp(start_timestamp)
         end_date = datetime.fromtimestamp(end_timestamp)
-
         records = Record.objects.filter(date__gte=start_date, date__lte=end_date, currency_code="('{0}',)".format(code_to))
-
         return records
 
 schema = Schema(query=Query)
